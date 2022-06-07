de3d470b9aa04058545d7bd304b9c24e120b3e8a
solomontsumah <99472574+Tsumahhh@users.noreply.github.com>
2022-06-07 18:31:22 +0300

    Mean

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for dig in my_list:
-        print(f'{dig:d}')
+    for z in range(len(my_list)):
+        print("{:d}".format(my_list[z]))
